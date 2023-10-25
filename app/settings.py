a6a34ac206b5cef6786f1618e15d45f78f35d82e
raifran1 <raifranlucas@outlook.com>
2023-10-25 15:37:48 -0300

    feat: adicionando configuração para paginação nativa do Rest Framework

--- app/settings.py
+++ app/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "app.wsgi.application"
 
+# Paginação configurada
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
